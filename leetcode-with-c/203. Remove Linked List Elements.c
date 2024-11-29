98cbc505b5cd72909e436a8fe8e117a8f6e9c60f
PikachuTW <chenyenting0830@gmail.com>
2024-11-29 21:58:34 +0800

    some leetcode problems

---
+++ leetcode-with-c/203. Remove Linked List Elements.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode** indirect = &head;
+    while (*indirect != NULL) {
+        if ((*indirect)->val != val) {
+            indirect = &((*indirect)->next);
+        } else {
+            *indirect = (*indirect)->next;
+        }
+    }
+    return head;
+}
